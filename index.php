3d8e4e54d70dddb53fdc8410258da05782fb61c7
leo-demetrio <leocdemetrio@gmail.com>
2021-01-12 12:23:49 -0300

    class Page tamplate ok

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+//session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -14,111 +14,111 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new Page();
-
 	$page->setTpl("index");
+	//chama o footer no destruct automaticamente
    
 });
 
-$app->get('/admin', function() {
+// $app->get('/admin', function() {
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-	$page = new PageAdmin();
+// 	$page = new PageAdmin();
 
-	$page->setTpl("index");
+// 	$page->setTpl("index");
    
-});
+// });
 
-$app->get('/admin/login', function() {
+// $app->get('/admin/login', function() {
 
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
+// 	$page = new PageAdmin([
+// 		"header"=>false,
+// 		"footer"=>false
+// 	]);
 
-	$page->setTpl("login");
+// 	$page->setTpl("login");
    
-});
+// });
 
-$app->post('/admin/login', function(){
+// $app->post('/admin/login', function(){
 
-	User::login($_POST["login"], $_POST["password"]);
+// 	User::login($_POST["login"], $_POST["password"]);
 
-	header("location: /admin");
-	exit;
+// 	header("location: /admin");
+// 	exit;
 
-});
+// });
 
-$app->get('/admin/logout', function(){
+// $app->get('/admin/logout', function(){
 
-	User::logout();
+// 	User::logout();
 
-	header("Location: /admin/login");
-	exit;
+// 	header("Location: /admin/login");
+// 	exit;
 
-});
+// });
 
-$app->get("/admin/users", function(){
+// $app->get("/admin/users", function(){
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-	$users = User::listAll();
+// 	$users = User::listAll();
 
-	$page = new PageAdmin();
+// 	$page = new PageAdmin();
 
-	$page->setTpl("users", array(
-		"users"=>$users		
-	));
+// 	$page->setTpl("users", array(
+// 		"users"=>$users		
+// 	));
 
-});
+// });
 
-$app->get("/admin/users/create", function(){
+// $app->get("/admin/users/create", function(){
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-	$page = new PageAdmin();
+// 	$page = new PageAdmin();
 
-	$page->setTpl("users-create");
+// 	$page->setTpl("users-create");
 
-});
+// });
 
-$app->get("/amin/users/:iduser/delete", function($iduser){
+// $app->get("/amin/users/:iduser/delete", function($iduser){
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-});
+// });
 
-$app->get("/admin/users/:iduser", function($iduser){
+// $app->get("/admin/users/:iduser", function($iduser){
 	
-	User::verifyLogin();
-	echo $iduser;
-	$page = new PageAdmin();
+// 	User::verifyLogin();
+// 	echo $iduser;
+// 	$page = new PageAdmin();
 
-	$page->setTpl("users-update");
+// 	$page->setTpl("users-update");
 
-});
+// });
 
-$app->post("/admin/users/create", function(){
+// $app->post("/admin/users/create", function(){
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-	$user = new User();
+// 	$user = new User();
 
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+// 	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
-	$user->setData($_POST);
+// 	$user->setData($_POST);
 
-	$user->save();
+// 	$user->save();
 
-	header("Location: /admin/users");
-	exit;
-});
+// 	header("Location: /admin/users");
+// 	exit;
+// });
 
-$app->post("/amin/users/:iduser", function($iduser){
+// $app->post("/amin/users/:iduser", function($iduser){
 
-	User::verifyLogin();
+// 	User::verifyLogin();
 
-});
+// });
 
 
 
---
+++ views-cache/cabecalho.2133d818ca8d7a49b3b1801ed47816d9.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+	<title>Testes</title>
+</head>
+<body>
+
+</body>
+</html>
