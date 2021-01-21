90e1200d5cb94bd2b963bd76bc8788ccee8f889e
leo-demetrio <leocdemetrio@gmail.com>
2021-01-21 15:48:02 -0300

    pequena correção

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $app->get('/admin', function() {
 	$page->setTpl("index");
    
 });
-
+//login
 $app->get('/admin/login', function() {
 
 	$page = new PageAdmin([
@@ -163,6 +163,7 @@ $app->get("/admin/forgot", function() {
 	$page->setTpl("forgot");
 
 });
+
 $app->post("/admin/forgot", function() {
 	
 	$user = User::getForgot($_POST["email"]);
@@ -170,9 +171,8 @@ $app->post("/admin/forgot", function() {
 	header("Location: /admin/forgot/sent");
 	exit;
 });
-$app->get('/admin/forgot/sent', function () {
 
-	
+$app->get('/admin/forgot/sent', function () {	
 
 	$page = new PageAdmin([
 			"header"=>false,
@@ -182,6 +182,47 @@ $app->get('/admin/forgot/sent', function () {
 	$page->setTpl("forgot-sent");
 
 });
+
+$app->get('/admin/forgot/reset', function () {
+
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+	$page->setTpl("forgot-reset", [
+
+	 "name" => $user["desperson"],
+	 "code" => $_GET["code"]
+
+	]);
+
+});
+
+$app->post('/admin/forgot/reset', function () {
+
+	$forgot = User::validForgotDecrypt($_POST['code']);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot['iduser']);
+
+	$user->setPassword($_POST["password"]);
+
+	$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
 
 $app->run();
 	
